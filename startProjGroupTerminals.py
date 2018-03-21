019c9b8976f67e53d272fe5427f9e2728a66726e
JC Jackson <jcjackson@jcjacksonconsulting.com>
2018-03-21 03:02:15 -0700

    first ided

---
+++ startProjGroupTerminals.py
@@ -0,0 +1,11 @@
+# startProjGroupTerminals.py 
+
+'''
+~/Dropbox/WkspcErlangElixir/BookErlangElixirImperativeProg
+~/Dropbox/WkspcNodeJS-Nodeschool/learnyounode
+~/Dropbox/WkspcPythonSTANDALONES/ReAppAnEmptyUbu
+~/Dropbox/WkspcRforDataAnalysis/BookRforDataScience
+~/Dropbox/WkspcWebAndApps/ClickHigh
+~/Dropbox/WkspcWebAndApps/MolecularMalady
+~/Dropbox/WkspcWebAndApps/wellKnowIt
+'''
