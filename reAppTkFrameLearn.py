4628fb833df0168e0f5da6999be59d09fbb547c2
JC Jackson <jcjackson@jcjacksonconsulting.com>
2021-04-21 08:41:57 +0000

    Finally found example of tk with multiprocessing

---
+++ reAppTkFrameLearn.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+multiLineComment='''
+# reAppTkFrameLearn.py
+# Copied from Slack Overflow web page for learning event driven messaging code
+# study by JC Jackson 
+'''
+
+multiLineComment='''
+
+'''
+
+# Try to import Python 2 name
+try:
+    import Tkinter as tk
+# Fall back to Python 3 if import fails
+except ImportError:
+    import tkinter as tk
+
+class Example(tk.Frame):
+    def __init__(self, root):
+        tk.Frame.__init__(self, root)
+        menubar = tk.Menu(self)
+        fileMenu = tk.Menu(self)
+        recentMenu = tk.Menu(self)
+
+        menubar.add_cascade(label="File", menu=fileMenu)
+        fileMenu.add_cascade(label="Open Recent", menu=recentMenu)
+        for name in ("file1.txt", "file2.txt", "file3.txt"):
+            recentMenu.add_command(label=name)
+
+
+        root.configure(menu=menubar)
+        root.geometry("200x200")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    Example(root).pack(fill="both", expand=True)
+    root.mainloop()
