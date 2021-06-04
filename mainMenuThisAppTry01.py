25f9ffa35d3465db208be610911704d8f13559d8
JC Jackson <jcjackson@joe-jackson.net>
2021-06-04 00:37:26 -0700

    Learning to make desired menus, so far we have a working simple file menu triplet

--- mainMenuThisAppTry01.py
+++ mainMenuThisAppTry01.py
@@ -4,12 +4,48 @@
 # '''
 # mainMenuThisAppTry01.py 
 
+
+import tkinter as tk
+from tkinter import (Tk, BOTH, Text, E, W, S, N, END, INSERT, 
+    NORMAL, DISABLED, StringVar, Menu)
+from tkinter.ttk import Frame, Label, Button, Progressbar, Entry
+from tkinter import scrolledtext as st
+
+win = tk.Tk() 
+
+import multiprocessing as mp
+from multiprocessing import Queue, Process, Pool
+import queue 
+from decimal import Decimal, getcontext
+
+import time
+
 #####################################################
 # class TextFrameTry02(Frame):
 class MainMenuThisApp(Frame):
-        print() 
+    print() 
 
+#####################################################
+# class TextFrameTry02(Frame):
+# class PiCruncher(Frame):
 
+    def __init__(self, parent, q):
+        Frame.__init__(self, parent)   
+         
+        # self.queue = q 
+        self.parent = parent
+        
+        # Status XInitThreads(void) 
+        # XInitThreads(void)
+        
+        self.initUI()
+
+    def callback(self):
+        self.root.quit()
+
+                
+    def initUI(self):
+      
         # self.root = tk.Tk()
         # self.root = Tk()
         # self.root.protocol("WM_DELETE_WINDOW", self.callback)
@@ -53,10 +89,62 @@ class MainMenuThisApp(Frame):
         self.txt.insert(INSERT, "Just set up this txt.\n\n") 
         # aKeyStrk = keyboard.read_key()
         # aKeyStrk = input() 
-
+        
 #####################################################################
-def main(): 
 
+
+#####################################################
+# class TextFrameTry02(Frame):
+def mainMenuThisApp():
+    print() 
+
+    # from Tkinter import *
+    
+    def donothing():
+       filewin = Toplevel(root)
+       button = Button(filewin, text="Do nothing button")
+       button.pack()
+       
+    root = Tk()
+    menubar = Menu(root)
+    filemenu = Menu(menubar, tearoff=0)
+    filemenu.add_command(label="New", command=donothing)
+    filemenu.add_command(label="Open", command=donothing)
+    filemenu.add_command(label="Save", command=donothing)
+    filemenu.add_command(label="Save as...", command=donothing)
+    filemenu.add_command(label="Close", command=donothing)
+    
+    filemenu.add_separator()
+    
+    filemenu.add_command(label="Exit", command=root.quit)
+    menubar.add_cascade(label="File", menu=filemenu)
+    editmenu = Menu(menubar, tearoff=0)
+    editmenu.add_command(label="Undo", command=donothing)
+    
+    editmenu.add_separator()
+    
+    editmenu.add_command(label="Cut", command=donothing)
+    editmenu.add_command(label="Copy", command=donothing)
+    editmenu.add_command(label="Paste", command=donothing)
+    editmenu.add_command(label="Delete", command=donothing)
+    editmenu.add_command(label="Select All", command=donothing)
+    
+    menubar.add_cascade(label="Edit", menu=editmenu)
+    helpmenu = Menu(menubar, tearoff=0)
+    helpmenu.add_command(label="Help Index", command=donothing)
+    helpmenu.add_command(label="About...", command=donothing)
+    menubar.add_cascade(label="Help", menu=helpmenu)
+    
+    xPosThisFrame=400 ; yPosThisFrame=200
+    dotGeoStr="400x350"+"+"+str(xPosThisFrame)+"+"+str(yPosThisFrame)
+    root.geometry(dotGeoStr) 
+
+    root.config(menu=menubar)
+    root.mainloop()
+#####################################################################
+
+def main(): 
+    print("") 
     # q = Queue()
     # queue = multiprocessing.Queue() 
     # args = (queue, queue)
